5ee3f9228a61d7c50c5f9578d88148bfa1f28a3e
Maria <msaav3@gmail.com>
2026-01-12 22:05:53 -0600

    chore: added cloud mesh

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import Background from "@/components/Background";
+import Cloud from "@/components/Cloud";
 import Synth from "@/components/Synth";
-
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans ">
-      <Synth/>
+      <Cloud />
+      <Synth />
       <Background />
     </div>
   );
---
+++ components/Cloud.tsx
@@ -0,0 +1,7 @@
+export default function Cloud() {
+  return (
+    <div className="rounded-lg p-4  border-2 border-white cursor-pointer h-60 w-60  mx-2">
+        <div className="bg-blue-600 rounded-md h-full w-full"></div>
+    </div>
+  );
+}
